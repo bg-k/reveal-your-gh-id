41f120b92b9a945d275b4897de147167dbccdec9
ryo makabe <penginryo@gmail.com>
2020-08-05 22:00:12 -0700

    update wording

--- src/components/ApiNavigator.js
+++ src/components/ApiNavigator.js
@@ -4,7 +4,7 @@ import {SEARCH_URL} from "../api";
 const ApiNavigator = () => {
   return(
     <p >
-      Don't believe? Let's check it in the <a href={SEARCH_URL} target="_blank">GitHub api with your user name.</a>
+      Don't believe? Let's check it in the <a href={SEARCH_URL} target="_blank">GitHub api.</a>
     </p>
   );
 };
